cfb5f2483c672c5ec7177fb558d13089fbea931e
Soichi Hayashi <soichih@gmail.com>
2016-05-16 11:12:36 -0400

    massaging pakcage.json

--- app.ts
+++ app.ts
@@ -54,8 +54,6 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-console.log("registering");
 electron.ipcMain.on('show-console', (e) => {
   //console.log("show-consolle called");
   win.webContents.openDevTools();
