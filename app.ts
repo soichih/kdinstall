190aad388d07b42565bfcaaf87821ec57d787868
Soichi Hayashi <soichih@gmail.com>
2016-05-16 11:38:33 -0400

    added frame: false

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 800, height: 600, frame: false});
   
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`);
